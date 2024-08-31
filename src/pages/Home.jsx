7a2afd26f377a5e1e786bcaf595ef1d63e96f474
ReactNativeAmalgum <137068385+ReactNativeAmalgum@users.noreply.github.com>
2024-08-31 17:10:56 +0530

    4th commit 31-8-24

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,10 @@ import CarouselBanner from './inc/CarouselBanner'
 import MoreMember from './inc/MoreMember'
 import HomeService from './inc/HomeService'
 import HomeMarkq from './inc/HomeMarkq'
-import HomeOutstanding from './inc/HomeOutstanding'
 import HomeClassWork from './inc/HomeClassWork'
 import HomeTween from './inc/HomeTween'
 import HomeTranies from './inc/HomeTranies'
 import HomeSignup from './inc/HomeSignup'
-import HomeTestimonial from './inc/HomeTestimonial'
 import HomeReview from './inc/HomeReview'
 
 export default function Home() {
